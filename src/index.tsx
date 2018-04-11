b8ebec6545f3d5ffde466428b6055e033efaeaed
Andrew Morris <voltrevo@gmail.com>
2018-04-11 11:48:18 +0000

    Implement filter highlighting and restore load state functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Router from './Router';
-// import Constants from './Constants';
+import * as GlobalConstants from './GlobalConstants';
 import registerServiceWorker from './registerServiceWorker';
 import './style.css';
 
 import * as App from './App';
 
-// const { SET_WHOLE_STATE, APP_STATE } = Constants.Global;
+const { SET_WHOLE_STATE, APP_STATE } = GlobalConstants;
 
 const seed = `${Math.random()}`;
 const store = App.Store(seed);
 
-// const storedState = localStorage.getItem(APP_STATE);
+const storedState = localStorage.getItem(APP_STATE);
 
-// if (storedState) {
-//   store.Dispatcher({ type: SET_WHOLE_STATE, data: JSON.parse(storedState) })();
-// }
+if (storedState && location.href.indexOf('loadState') !== -1) {
+  store.Dispatcher({ type: SET_WHOLE_STATE, data: JSON.parse(storedState) })();
+}
 
-// store.subscribe(() => {
-//   localStorage.setItem(
-//     APP_STATE,
-//     JSON.stringify(store.getState())
-//   );
-// });
+store.subscribe(() => {
+  localStorage.setItem(
+    APP_STATE,
+    JSON.stringify(store.getState())
+  );
+});
 
 const render = () => {
   ReactDOM.render(
