e7096343405da5bd8be285e4c3a96a5edc16d930
Andrew Morris <voltrevo@gmail.com>
2018-04-11 09:32:17 +0000

    Remove unnecessary ActionCreator usage

--- src/Components/Counter/Actions.ts
+++ src/Components/Counter/Actions.ts
@@ -1,14 +1,13 @@
-import { ActionCreator } from 'redux';
 import * as Types from './Types';
 import * as Constants from './Constants';
 
 // Define actions
-export const Increment: ActionCreator<Types.IncrementCounterAction> = (amount: number) => ({
+export const Increment = (amount: number): Types.IncrementCounterAction => ({
     type: Constants.INCREMENT,
     data: amount
 });
 
-export const Decrement: ActionCreator<Types.DecrementCounterAction> = (amount: number) => ({
+export const Decrement = (amount: number): Types.DecrementCounterAction => ({
     type: Constants.DECREMENT,
     data: amount
 });
